60bbc9a9e59d6894ac0d32eff96df0033273a4b2
rolanjose7 <rolanjose7@hotmail.com>
2023-01-05 15:51:28 -0400

    Added the game function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getComputerChoice(){
     let computerPick = Math.floor(Math.random() * 3 + 1);
     if(computerPick == 1){
         return "rock";
-    } else if (computerpick == 2){
+    } else if (computerPick == 2){
         return "paper";
     } else {
         return "scissors";
@@ -13,3 +13,35 @@ function getPlayerChoice(){
     let playerPick = prompt("Rock Paper Scissors?").toLowerCase();
     return playerPick;
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock"){
+        if(computerSelection == "paper"){
+            return "Computer Wins!";
+        } else if (computerSelection == "scissors"){
+            return "Player Wins!";
+        } else {
+            return "It's a tie!";
+        }
+    }
+    if(playerSelection == "paper"){
+        if(computerSelection == "scissors"){
+            return "Computer Wins!";
+        } else if (computerSelection == "rock"){
+            return "Player Wins!";
+        } else {
+            return "It's a tie!";
+        }
+    }
+    if(playerSelection == "scissors"){
+        if(computerSelection == "paper"){
+            return "Computer Wins!";
+        } else if (computerSelection == "rock"){
+            return "Player Wins!";
+        } else {
+            return "It's a tie!";
+        }
+    }
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
